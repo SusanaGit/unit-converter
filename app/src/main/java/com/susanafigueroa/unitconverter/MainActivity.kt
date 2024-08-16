69bac54e8947e9744861926fe41f87e2632e2a17
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-16 17:23:22 +0200

    add padding

--- app/src/main/java/com/susanafigueroa/unitconverter/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/unitconverter/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.susanafigueroa.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,12 +40,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun unitConverter(){
-    Column{
-        Row{
-            Text("Row1")
-        }
-        Row{
-            Text("Row2")
+    BoxWithConstraints {
+        val padding = maxWidth * 0.10f
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ){
+            Text("Unit Converter")
+            OutlinedTextField(value = "", onValueChange = {
+
+            })
+            Row{
+
+            }
+            Row{
+                Text("Row2")
+            }
         }
     }
 }
