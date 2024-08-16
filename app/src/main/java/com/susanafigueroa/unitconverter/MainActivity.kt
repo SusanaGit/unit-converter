ec6d6cec8c7532e9a273049faf0ef618d0311d8d
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-16 16:52:53 +0200

    add unitConverter method

---
+++ app/src/main/java/com/susanafigueroa/unitconverter/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.susanafigueroa.unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.susanafigueroa.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnitConverterTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    unitConverter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun unitConverter(){
+    Column{
+
+        Greeting("Android")
+        Greeting("Android")
+        Row{
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UnitConverterTheme {
+        Greeting("Android")
+    }
+}
