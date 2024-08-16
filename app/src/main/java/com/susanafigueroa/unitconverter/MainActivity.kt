d002c00060c71a3e091cfacb9d1fbca9ce22a582
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-16 22:08:21 +0200

    control expanded menus

--- app/src/main/java/com/susanafigueroa/unitconverter/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/unitconverter/MainActivity.kt
@@ -1,36 +1,16 @@
 package com.susanafigueroa.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.susanafigueroa.unitconverter.ui.theme.UnitConverterTheme
@@ -68,12 +48,20 @@ fun UnitConverter(){
         Spacer(modifier = Modifier.height(16.dp))
 
         Row{
+
             Box {
-                Button(onClick = {}) {
+
+                var expanded_menu by remember {
+                    mutableStateOf(false)
+                }
+
+                Button(onClick = { expanded_menu = true }) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(
+                    expanded = expanded_menu,
+                    onDismissRequest = { expanded_menu = false }) {
                     DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
                     DropdownMenuItem(text = { Text("Meters") }, onClick = { /*TODO*/ })
                     DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
@@ -84,11 +72,18 @@ fun UnitConverter(){
             Spacer(modifier = Modifier.width(16.dp))
 
             Box {
-                Button(onClick = {}) {
+
+                var expanded_menu by remember {
+                    mutableStateOf(false)
+                }
+
+                Button(onClick = { expanded_menu = true }) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(
+                    expanded = expanded_menu,
+                    onDismissRequest = { expanded_menu = false }) {
                     DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
                     DropdownMenuItem(text = { Text("Meters") }, onClick = { /*TODO*/ })
                     DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
@@ -96,16 +91,6 @@ fun UnitConverter(){
                 }
             }
         }
-        /*Row{
-            val context = LocalContext.current
-            Button(onClick = { Toast
-                .makeText(context,
-                    "Thanks for clicking!",
-                    Toast.LENGTH_LONG).show() })
-            {
-                Text("Click Me!")
-            }
-        }*/
 
         Spacer(modifier = Modifier.height(16.dp))
 
